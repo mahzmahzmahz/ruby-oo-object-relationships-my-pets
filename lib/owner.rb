67626304d858d8b48ebda17313a9b42fb0786598
Alex Marz <alexhmarz@gmail.com>
2020-12-10 16:55:50 -0500

    Draft 1

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,25 @@
+
+  require 'pry'
 class Owner
   # code goes here
+attr_reader :name, :species
+attr_writer 
+@@all = []
+  def initialize(name) 
+    @name = name
+    @species = "human"
+    @@all << self 
+  end
+  def say_species
+    "I am a #{self.species}."
+  end
+  def self.all
+    @@all
+  end
+  def self.count
+    @@all.length
+  end
+  def self.reset_all
+    @@all = []
+  end
 end
